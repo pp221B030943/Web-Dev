a6476de935a95e72c01781482b32908d365b7111
pp221B030943 <a_fedoseenko@kbtu.kz>
2023-02-21 16:12:58 +0600

    .

--- Shop/src/app/app.module.ts
+++ Shop/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule
