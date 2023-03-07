a87199826e5bf38ab8fafac43df17ea2ff1f493c
pp221B030943 <a_fedoseenko@kbtu.kz>
2023-03-07 17:33:52 +0600

    album photos updates

---
+++ Lab6/src/app/album-photos/album-photos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Album, Photo} from "../models";
+import {AlbumService} from "../album.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-album-photos',
+  templateUrl: './album-photos.component.html',
+  styleUrls: ['./album-photos.component.css']
+})
+export class AlbumPhotosComponent implements OnInit{
+  photos: Photo[] = []
+
+  constructor(private route: ActivatedRoute,
+              private albumService: AlbumService) {
+    this.photos = [];
+  }
+
+  ngOnInit(){
+
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.albumService.getPhotos(id).subscribe((photos) => {
+        this.photos = photos;
+      });
+    })
+  }
+
+
+}
