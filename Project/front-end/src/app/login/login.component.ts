5193adb109aae45744cb824cacca83dbb1b83c61
pp221B030943 <a_fedoseenko@kbtu.kz>
2023-04-21 15:55:20 +0600

    more corrections for frontend

--- Project/front-end/src/app/login/login.component.ts
+++ Project/front-end/src/app/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  isLogin: boolean = true;
+  credentials: Credentials = { email: '', password: '' };
+  registration: Registration = { name: '', email: '', password: '', confirmPassword: '' };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleIsLogin(): void {
+    this.isLogin = !this.isLogin;
+  }
+
+  login(): void {
+    // TODO: implement login logic
+    console.log('Logging in with:', this.credentials);
+  }
+
+  register(): void {
+    // TODO: implement registration logic
+    console.log('Registering user:', this.registration);
+  }
+
+}
+
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+interface Registration {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }
--- Shop/src/app/products.ts
+++ Shop/src/app/products.ts
@@ -1,100 +1,99 @@
 export interface Product {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
+  id: number;
+  name: string;
+  price: number;
+  description: string;
 }
-  
+
 export const products = [
-    {
-      id: 1,
-      name: 'Tom Ford Tobacco Vanille',
-      price: 300,
-      description: 'Tom Ford Tobacco Vanille is an amazing spicy–oriental tandem equally good for use by women and men',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h09/h66/51703766974494/tom-ford-tobacco-vanille-parfumernaa-voda-edp-5-ml-17303307-2.jpg",
-      url: "https://kaspi.kz/shop/p/tom-ford-tobacco-vanille-parfjumernaja-voda-edp-5-ml-17303307/?c=750000000#!/item",
-      rating: 5
-    },
-    {
-      id: 2,
-      name: 'Tom Ford Soleil Blanc',
-      price: 290,
-      description: 'The new fragrance of the Tom Ford Private Blend collection opens the way to the world of warmth and luxury, the embodiment of which was the Tom Ford Soleil collection.',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/h90/32255864700958/tom-ford-soleil-blanc-edp-50-ml-17302998-3-Container.jpg",
-      url: "https://kaspi.kz/shop/p/tom-ford-soleil-blanc-parfjumernaja-voda-edp-50-ml-17302998/?c=750000000#!/item",
-      rating: 4
-    },
-    {
-      id: 3,
-      name: 'Juliette Has a Gun Miss Charming',
-      price: 390,
-      description: 'Floral feminine fragrance', 
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6b/hc9/34377285337118/juliette-has-a-gun-miss-charming-100-ml-101282475-1.jpg",
-      url: "https://kaspi.kz/shop/p/juliette-has-a-gun-miss-charming-parfjumernaja-voda-edp-100-ml-101282475/?c=750000000#!/item",
-      rating: 3
-    },
-    {
-      id: 4,
-      name: 'Juliette Has a Gun Musc Invisible',
-      price: 320,
-      description: 'The best musky fragrance',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/heb/h95/47037142663198/juliette-has-a-gun-musc-invisible-50-ml-102809076-1.jpg",
-      url: "https://kaspi.kz/shop/p/juliette-has-a-gun-musc-invisible-parfjumernaja-voda-edp-50-ml-102809076/?c=750000000#!/item",
-      rating: 5,
-    },
-    {
-      id: 5,
-      name: 'Jo Malone Wild Bluebell',
-      price: 250,
-      description: 'Light summer feminine fragrance',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h48/h45/33293373046814/jo-malone-wild-bluebell-edt-50-ml-100814077-1.jpg",
-      url: "https://kaspi.kz/shop/p/jo-malone-wild-bluebell-tualetnaja-voda-edt-50-ml-100814077/?c=750000000#!/item",
-      rating: 5
-    },
-    {
-      id: 6,
-      name: 'Jo Malone WOOD SAGE&SEA SALT ',
-      price: 340,
-      description: 'The smell of the sea is deep, with salty splashes, natural and therefore very comfortable.',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4d/h9a/51995500412958/jo-malone-wood-sage-sea-salt-30ml-edc-101168567-1-Container.jpg",
-      url: "https://kaspi.kz/shop/p/jo-malone-wood-sage-sea-salt-edc-tualetnaja-voda-edt-30-ml-101168567/?c=750000000#!/item",
-      rating: 5
-    },
-    {
-      id: 7,
-      name: 'Jo Malone English Pear & Freesia EDC',
-      price: 200,
-      description: 'The best from the lemon section',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3c/h2a/32252670541854/jo-malone-english-pear-freesia-edc-100-ml-17302994-1-Container.jpg",
-      url: "https://kaspi.kz/shop/p/jo-malone-english-pear-freesia-edc-odekolon-edc-100-ml-17302994/?c=750000000#!/item",
-      rating: 5
-    },
-    {
-      id: 8,
-      name: 'Juliette Has a Gun Sunny Side Up',
-      price: 180,
-      description: 'France Fragrance',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha9/hfc/32382816813086/juliette-has-a-gun-sunny-side-up-edp-50-ml-100110118-1.jpg",
-      url : "https://kaspi.kz/shop/p/juliette-has-a-gun-sunny-side-up-parfjumernaja-voda-edp-50-ml-100110118/?c=750000000#!/item",
-      rating: 3
-    },
-    {
-      id: 9,
-      name: 'Jo Malone Lime Basil & Mandarin',
-      price: 165,
-      description: 'Jo Malone Lime Basil Mandarin perfume is a perfume with a complex composition of many components',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hde/h67/48988041347102/jo-malone-lime-basil-mandarin-odekolon-edc-50-ml-103885824-1.jpg",
-      url: "https://kaspi.kz/shop/p/jo-malone-lime-basil-mandarin-odekolon-edc-50-ml-103885824/?c=750000000#!/item",
-      rating: 3
-    },
-    {
-      id: 10,
-      name: 'Juliette Has a Gun Pear Inc.',
-      price: 399,
-      description: 'The best fragrance',
-      img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4b/hc7/47130015465502/juliette-has-a-gun-parfumirovannaa-voda-pear-inc-50-ml-102855344-1.jpg",
-      url: "https://kaspi.kz/shop/p/juliette-has-a-gun-parfjumirovannaja-voda-pear-inc-parfjumernaja-voda-edp-50-ml-102855344/?c=750000000#!/item",
-      rating: 4
-    }
-  ];
-  
+  {
+    id: 1,
+    name: 'Tom Ford Tobacco Vanille',
+    price: 300,
+    description: 'Tom Ford Tobacco Vanille is an amazing spicy–oriental tandem equally good for use by women and men',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h09/h66/51703766974494/tom-ford-tobacco-vanille-parfumernaa-voda-edp-5-ml-17303307-2.jpg",
+    url: "https://kaspi.kz/shop/p/tom-ford-tobacco-vanille-parfjumernaja-voda-edp-5-ml-17303307/?c=750000000#!/item",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: 'Tom Ford Soleil Blanc',
+    price: 290,
+    description: 'The new fragrance of the Tom Ford Private Blend collection opens the way to the world of warmth and luxury, the embodiment of which was the Tom Ford Soleil collection.',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/h90/32255864700958/tom-ford-soleil-blanc-edp-50-ml-17302998-3-Container.jpg",
+    url: "https://kaspi.kz/shop/p/tom-ford-soleil-blanc-parfjumernaja-voda-edp-50-ml-17302998/?c=750000000#!/item",
+    rating: 4
+  },
+  {
+    id: 3,
+    name: 'Juliette Has a Gun Miss Charming',
+    price: 390,
+    description: 'Floral feminine fragrance', 
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6b/hc9/34377285337118/juliette-has-a-gun-miss-charming-100-ml-101282475-1.jpg",
+    url: "https://kaspi.kz/shop/p/juliette-has-a-gun-miss-charming-parfjumernaja-voda-edp-100-ml-101282475/?c=750000000#!/item",
+    rating: 3
+  },
+  {
+    id: 4,
+    name: 'Juliette Has a Gun Musc Invisible',
+    price: 320,
+    description: 'The best musky fragrance',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/heb/h95/47037142663198/juliette-has-a-gun-musc-invisible-50-ml-102809076-1.jpg",
+    url: "https://kaspi.kz/shop/p/juliette-has-a-gun-musc-invisible-parfjumernaja-voda-edp-50-ml-102809076/?c=750000000#!/item",
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: 'Jo Malone Wild Bluebell',
+    price: 250,
+    description: 'Light summer feminine fragrance',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h48/h45/33293373046814/jo-malone-wild-bluebell-edt-50-ml-100814077-1.jpg",
+    url: "https://kaspi.kz/shop/p/jo-malone-wild-bluebell-tualetnaja-voda-edt-50-ml-100814077/?c=750000000#!/item",
+    rating: 5
+  },
+  {
+    id: 6,
+    name: 'Jo Malone WOOD SAGE&SEA SALT ',
+    price: 340,
+    description: 'The smell of the sea is deep, with salty splashes, natural and therefore very comfortable.',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4d/h9a/51995500412958/jo-malone-wood-sage-sea-salt-30ml-edc-101168567-1-Container.jpg",
+    url: "https://kaspi.kz/shop/p/jo-malone-wood-sage-sea-salt-edc-tualetnaja-voda-edt-30-ml-101168567/?c=750000000#!/item",
+    rating: 5
+  },
+  {
+    id: 7,
+    name: 'Jo Malone English Pear & Freesia EDC',
+    price: 200,
+    description: 'The best from the lemon section',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3c/h2a/32252670541854/jo-malone-english-pear-freesia-edc-100-ml-17302994-1-Container.jpg",
+    url: "https://kaspi.kz/shop/p/jo-malone-english-pear-freesia-edc-odekolon-edc-100-ml-17302994/?c=750000000#!/item",
+    rating: 5
+  },
+  {
+    id: 8,
+    name: 'Juliette Has a Gun Sunny Side Up',
+    price: 180,
+    description: 'France Fragrance',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha9/hfc/32382816813086/juliette-has-a-gun-sunny-side-up-edp-50-ml-100110118-1.jpg",
+    url : "https://kaspi.kz/shop/p/juliette-has-a-gun-sunny-side-up-parfjumernaja-voda-edp-50-ml-100110118/?c=750000000#!/item",
+    rating: 3
+  },
+  {
+    id: 9,
+    name: 'Jo Malone Lime Basil & Mandarin',
+    price: 165,
+    description: 'Jo Malone Lime Basil Mandarin perfume is a perfume with a complex composition of many components',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hde/h67/48988041347102/jo-malone-lime-basil-mandarin-odekolon-edc-50-ml-103885824-1.jpg",
+    url: "https://kaspi.kz/shop/p/jo-malone-lime-basil-mandarin-odekolon-edc-50-ml-103885824/?c=750000000#!/item",
+    rating: 3
+  },
+  {
+    id: 10,
+    name: 'Juliette Has a Gun Pear Inc.',
+    price: 399,
+    description: 'The best fragrance',
+    img : "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4b/hc7/47130015465502/juliette-has-a-gun-parfumirovannaa-voda-pear-inc-50-ml-102855344-1.jpg",
+    url: "https://kaspi.kz/shop/p/juliette-has-a-gun-parfjumirovannaja-voda-pear-inc-parfjumernaja-voda-edp-50-ml-102855344/?c=750000000#!/item",
+    rating: 4
+  }
+];
